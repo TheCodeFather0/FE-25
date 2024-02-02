02d3ea9f473f76d516e0fb482843c707ef925ddf
TheCodeFather0 <raminmammadov056@gmail.com>
2024-02-02 21:02:54 +0400

    'js-1'

---
+++ Lesson 18 (js-1)/app.js
@@ -0,0 +1,65 @@
+"use strict";
+
+// console.log("HI fe-25!");
+// console.log("ad:", "ramin");
+// console.log("cavab: ", 2 + 2);
+
+// let qrupunAdi = "FE-25";
+// qrupunAdi = "FE-26";
+// console.log("HI", qrupunAdi);
+
+// let firstName = "Ramin";
+// let age = 42;
+// let message =
+//   "salam! menim adim " + firstName + ".Menim " + age + " yasim var.";
+// console.log(message);
+
+let carMarka = "MERCEDESS";
+let carModel = "E350";
+const carYear = 2008;
+// carYear = 2014;
+console.log("avtomobilin adi:", carMarka);
+console.log("avtomobilin modeli:", carModel);
+console.log("avtomobilin ili:", carYear);
+
+/*
+    HELLO WORLD -> UPPER CASE
+    hello world -> lower case
+    hello-world -> kebab-case
+    
+    HelloWorld  -> PascalCase
+    helloWorld  -> camelCase
+    hello_world -> snake_case
+*/
+
+// console.log("--------------");
+// let ad = "eldar";
+// let Ad = "samir";
+// let aD = "elmar";
+// console.log(ad);
+// console.log(Ad);
+// console.log(aD);
+
+let userName = "TheCodeFather0";
+const dogulduguIl = 1778;
+const SUYUN_QAYNAMA_TEMPRATURU = 100;
+// SUYUN_QAYNAMA_TEMPRATURU
+
+/*
+    VAR  -> OLD METHOD
+    LET  -> NEW METHOD
+    CONST-> FOR CONSTANT
+*/
+
+// const agacin_adi = "cinar";
+// console.log(agacin_adi);
+
+// let x = prompt("adin nedir ey beni Adem?");
+// let yasin_boyukdurmu_18den = confirm(
+//   "qardasoglu senin yasin 18den boyukdurmu?"
+// );
+// console.log(yasin_boyukdurmu_18den);
+// console.log("salam deyerli:", x);
+
+let soz = "salam olsun yaxsi qardaslara";
+console.log(soz.length);
