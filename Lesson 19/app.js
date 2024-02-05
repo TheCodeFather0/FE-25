f463ec764a70848294223d8709ea333913f1de53
TheCodeFather0 <raminmammadov056@gmail.com>
2024-02-05 09:18:16 -0800

    lesson 19

---
+++ Lesson 19/app.js
@@ -0,0 +1,112 @@
+
+let firstName = 'Ahmad'; // String
+let age = 45; // number
+let isDeveloper = false
+// let sentence = 'o deyir ki: "hava cox gozeldir" '
+
+// let info = `${firstName}'s car.He say: "js is veri qud" `
+//string interpolation
+
+
+
+// console.log(typeof age)
+// const cem = 2+2
+// const netice = '2 + 2 = ' + cem
+// console.log(`hasil: ${2 * 2}`);
+
+
+/*
+    integer      -> 5
+    float-points -> 5.001
+*/
+
+
+
+
+
+// let username = prompt("adiniz daxil edin");
+// let message = `${username} xos geldiniz`
+// console.log(message);
+
+// console.log(typeof true);
+
+// let ad = prompt('adin nedir?')
+// console.log(ad);
+
+// let a = 8 * "9"
+// console.log(a);
+
+
+// console.log(`cavab: ${1 + 2}`);
+
+
+// let name = 'Jhon'
+// alert(`${name} + 2`) //
+// alert(name + "hello" * 2)
+// alert("salam" + name +`${name}`) // salam name jhon
+
+
+
+// console.log(false * -10 == false);
+
+// const yas18denboyukdurmu = confirm('yasin 18den coxdurmu?')
+
+// console.log(yas18denboyukdurmu);
+
+
+
+let x = "999.1";
+console.log(x);
+
+// x = String(x)
+// x = Boolean("") //true false
+// x = Number(x)
+// x = parseInt(x)
+// x = parseFloat(x)
+// console.log(+x);
+
+/*
+    truethy -> string, number, 1, true
+    falsy   -> 0, "", null, undefined, NaN, false
+
+
+    + hesablayir
+    + birlesdirir
+    + reqeme cevirir
+
+*/
+
+
+// console.log();
+
+// let a = +prompt('birinci reqemi daxil et')
+// let b = +prompt('ikinci reqemi daxil et') 
+// console.log(2 * (a + b));
+
+
+
+
+// let name = "Dan";
+// alert(`hello ${name + 1}`); // ?
+// "Dan" + 1 // Dan1
+
+// console.log(true + false);
+
+// console.log(6 / "3a");
+
+// console.log("2a" * "3");
+
+
+// console.log(4 + 5 + "px");
+
+// console.log("$" + 4 + 5);
+
+// console.log('4' - 2);
+// console.log("4px" - 2);
+// console.log(7 / 0);
+
+// console.log('-9 ' + 5)
+
+// console.log(-9 - 5);
+console.log(null + 1) 
+console.log(undefined + "1") 
