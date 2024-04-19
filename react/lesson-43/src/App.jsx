b2f2042fa4c4267d3aa0d3f4c7e3961c727e9c0d
TheCodeFather0 <raminmammadov056@gmail.com>
2024-04-19 21:08:04 +0400

    first react lesson

--- react/lesson-43/src/App.jsx
+++ react/lesson-43/src/App.jsx
@@ -1,16 +1,13 @@
 import "./css/style.css";
 import React from "react";
-import { data } from "./data";
-import Card from "./Components/Card";
+import Info from "./Components/Info";
+import Cards from "./Components/Cards";
 
 const App = () => {
   return (
     <div className="mainContent">
-      <div className="container">
-        {data.map(({ id, title, desc, imgSrc }) => {
-          return <Card key={id} title={title} desc={desc} imgSrc={imgSrc} />;
-        })}
-      </div>
+      <Cards />
+      <Info />
     </div>
   );
 };
